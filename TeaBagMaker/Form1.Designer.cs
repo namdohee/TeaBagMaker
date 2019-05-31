aa9ea0ecc5f57b7177cbc70242c2ac5afa54e373
dodohee <skaehgmlghgh@gmail.com>
2019-05-31 14:59:27 +0900

    폼 디자인 완료

--- TeaBagMaker/Form1.Designer.cs
+++ TeaBagMaker/Form1.Designer.cs
@@ -28,20 +28,75 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.tea = new System.Windows.Forms.ComboBox();
+            this.put = new System.Windows.Forms.Button();
+            this.count = new System.Windows.Forms.Label();
+            this.namdohee = new System.Windows.Forms.Label();
             this.SuspendLayout();
+            // 
+            // tea
+            // 
+            this.tea.Cursor = System.Windows.Forms.Cursors.WaitCursor;
+            this.tea.FormattingEnabled = true;
+            this.tea.Items.AddRange(new object[] {
+            "홍차",
+            "녹차",
+            "루이보스차",
+            "국화차"});
+            this.tea.Location = new System.Drawing.Point(13, 13);
+            this.tea.Name = "tea";
+            this.tea.Size = new System.Drawing.Size(256, 20);
+            this.tea.TabIndex = 0;
+            // 
+            // put
+            // 
+            this.put.Location = new System.Drawing.Point(13, 135);
+            this.put.Name = "put";
+            this.put.Size = new System.Drawing.Size(256, 49);
+            this.put.TabIndex = 1;
+            this.put.Text = "담그기!";
+            this.put.UseVisualStyleBackColor = true;
+            // 
+            // count
+            // 
+            this.count.AutoSize = true;
+            this.count.Location = new System.Drawing.Point(123, 82);
+            this.count.Name = "count";
+            this.count.Size = new System.Drawing.Size(36, 12);
+            this.count.TabIndex = 2;
+            this.count.Text = "count";
+            // 
+            // namdohee
+            // 
+            this.namdohee.AutoSize = true;
+            this.namdohee.Location = new System.Drawing.Point(12, 195);
+            this.namdohee.Name = "namdohee";
+            this.namdohee.Size = new System.Drawing.Size(69, 12);
+            this.namdohee.TabIndex = 3;
+            this.namdohee.Text = "3106 남도희";
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(281, 345);
+            this.ClientSize = new System.Drawing.Size(281, 216);
+            this.Controls.Add(this.namdohee);
+            this.Controls.Add(this.count);
+            this.Controls.Add(this.put);
+            this.Controls.Add(this.tea);
             this.Name = "Form1";
             this.Text = "Form1";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.ComboBox tea;
+        private System.Windows.Forms.Button put;
+        private System.Windows.Forms.Label count;
+        private System.Windows.Forms.Label namdohee;
     }
 }
 
